680a89b518dc78e74bab721c81174e9c85dd8d96
ryansyah17 <ryansyahputra553@gmail.com>
2022-11-17 19:07:06 +0700

    oke

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -15,8 +15,8 @@
                         <span class="count bg-success"></span>
                     </div>
                     <div class="profile-name">
-                        <h5 class="mb-0 font-weight-normal">Henry Klein</h5>
-                        <span>Gold Member</span>
+                        <h5 class="mb-0 font-weight-normal">Admin</h5>
+                        
                     </div>
                 </div>
                 <a href="#" id="profile-dropdown" data-toggle="dropdown"><i
@@ -111,41 +111,11 @@
             </a>
         </li>
         
-        <li class="nav-item menu-items">
-            <a class="nav-link" href="{{ url('/form') }}">
-                <span class="menu-icon">
-                    <i class="mdi mdi-playlist-play"></i>
-                </span>
-                <span class="menu-title">Form Elements</span>
-            </a>
-        </li>
+       
         
         
        
-        <li class="nav-item menu-items">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false"
-                aria-controls="auth">
-                <span class="menu-icon">
-                    <i class="mdi mdi-security"></i>
-                </span>
-                <span class="menu-title">User Pages</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank
-                            Page </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register
-                        </a></li>
-                </ul>
-            </div>
-        </li>
+        
         
     </ul>
 </nav>
